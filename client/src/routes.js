268fc61f432195a3ecdf52b46bca3ab850fd226e
blakeaolson <blakeaolson1@gmail.com>
2023-07-22 17:00:38 -0500

    added dependencies. removed team analysis route.

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import About from "./pages/about";
 import Rankings from "./pages/rankings";
-import Analysis from "./pages/analysis";
 import { AnimatePresence } from "framer-motion";
 
 function AnimatedRoutes() {
@@ -12,7 +11,6 @@ function AnimatedRoutes() {
     <AnimatePresence mode='wait'>
       <Routes key={location.pathname} location={location}>
         <Route path="/" element={<Rankings />} />
-        <Route path="/team-analysis" element={<Analysis />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </AnimatePresence>
