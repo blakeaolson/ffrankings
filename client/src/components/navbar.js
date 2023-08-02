9076b15a40479daf85d3dcfdef59f76baeca3382
blakeaolson <blakeaolson1@gmail.com>
2023-08-02 17:58:17 -0500

    removed light mode

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -58,7 +58,6 @@ export default function NavBar() {
               > About </Button>
             </HStack>
           </HStack>
-          <SwitchTheme></SwitchTheme>
         </Flex>
 
         {isOpen ? (
